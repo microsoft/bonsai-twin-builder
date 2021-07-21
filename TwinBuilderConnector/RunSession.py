b666e514bf846d7e0952dc5e79cbbc863e97a09e
Forrest Trepte <forrest@microsoft.com>
2021-07-20 17:51:19 -0700

    more detailed logging

--- TwinBuilderConnector/RunSession.py
+++ TwinBuilderConnector/RunSession.py
@@ -115,14 +115,15 @@ def RunSession(twin_model_file):
                 time.sleep(event.idle.callback_time)
                 print("Idling...")
             elif event.type == "EpisodeStart":
-                print(event.episode_start.config)
+                print(f"Episode Starting with config {event.episode_start.config}")
                 sim.episode_start(event.episode_start.config)
                 episode += 1
             elif event.type == "EpisodeStep":
+                print(f"Stepping with action {event.episode_step.action}")
                 iteration += 1
                 sim.episode_step(event.episode_step.action)
             elif event.type == "EpisodeFinish":
-                print("Episode Finishing...")
+                print("Episode Finishing")
                 sim.episode_finish()
                 iteration = 0
             elif event.type == "Unregister":
